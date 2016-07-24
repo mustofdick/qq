d480e1e8042bfdb5750db9bab29636b57cbe11e8
Mihail Simbirsky <simbirsky@bk.ru>
2016-07-24 12:21:30 +0300

    ss

---
+++ src/qq/qq.java
@@ -0,0 +1,13 @@
+package qq;
+
+/**
+ * Created by Mihail on 7/24/2016.
+ */
+class main {
+    public static void main(String[] args) {
+        System.out.printf("qq sss\n");
+        System.out.printf("qq sss\n");
+        System.out.printf("qq sss\n");
+        System.out.printf("qq sss\n");
+    }
+}
